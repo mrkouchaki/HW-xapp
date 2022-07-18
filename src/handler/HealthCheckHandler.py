921436707813a265358f8f41975d53ffa7b4390e
mrkouchaki <93693562+mrkouchaki@users.noreply.github.com>
2022-07-18 12:05:08 -0500

    Update HealthCheckHandler.py

--- src/handler/HealthCheckHandler.py
+++ src/handler/HealthCheckHandler.py
@@ -22,17 +22,30 @@ from ._BaseHandler import _BaseHandler
 
 
 class HealthCheckHandler(_BaseHandler):
+    print('///////enter class HealthCheckHandler(_BaseHandler)///////')
 
     def __init__(self, rmr_xapp: RMRXapp, msgtype):
+        print('//////enter def init in HealthCheckHandler/////')
+        print('rmr_xapp=', rmr_xapp)
+        print('msgtype=', msgtype)
         super().__init__(rmr_xapp, msgtype)
         # self.sdl_alarm_mgr = SdlAlarmManager()
 
     def request_handler(self, rmr_xapp, summary, sbuf):
+        print('//////enter def request_handler in HealthCheckHandler/////')
+        print('sbuf=', sbuf)
+        print('summary=', summary)
         ok = self._rmr_xapp.healthcheck()
+        print('ok = self._rmr_xapp.healthcheck()=', ok)
         # self.sdl_alarm_mgr.checkSdl()
         if ok:
+            print('////enter if/////')
             payload = b"OK\n"
+            print('payload = b OK\n=', payload)
         else:
+            print('/////enter else//////')
             payload = b"ERROR [RMR or SDL is unhealthy]\n"
+            print('payload = b ERROR [RMR or SDL is unhealthy]\n=', payload)
+            print('Constants.RIC_HEALTH_CHECK_RESP=', Constants.RIC_HEALTH_CHECK_RESP)
         self._rmr_xapp.rmr_rts(sbuf, new_payload=payload, new_mtype=Constants.RIC_HEALTH_CHECK_RESP)
         self._rmr_xapp.rmr_free(sbuf)
